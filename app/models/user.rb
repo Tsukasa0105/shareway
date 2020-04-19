05220a0894041d316885941a90b737dd22960f06
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2020-04-19 10:13:38 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
     before_save { self.email.downcase! }
-    validates :name, presence: true, length: { maximum: 50 },
+    validates :name, presence: true, length: { in: 3..50 },
                      uniqueness: true
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
     
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
