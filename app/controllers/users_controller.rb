1416afcc56748a140e574276a76665e777406fc0
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2020-04-20 10:49:43 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.new(user_params)
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'ユーザー情報を変更しました'
       redirect_to root_path
@@ -80,6 +80,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :image)
+    params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
-    validates :password_confirmation, presence: true, length: { minimum: 6 }
     has_secure_password
 
     
