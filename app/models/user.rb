287027f66ef825343921a2e39c31df5472787b61
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2020-04-21 13:23:15 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, allow_blank: true, length: { minimum: 6 }
     has_secure_password
 
     
